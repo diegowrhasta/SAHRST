38abdb8c045796e25c032d110459d7e30bf9c6b4
Yawar Valeriano <yawarvs@gmail.com>
2019-09-08 17:52:45 -0400

    Added ConductorController (base file), Conductor & Ruta Models (initial files) Deleted credentials in .env.example (for DB security)

---
+++ app/Http/Controllers/ConductorController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Conductor;
+use Illuminate\Http\Request;
+
+class ConductorController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Conductor $conductor)
+    {
+        //
+    }
+
+    public function edit(Conductor $conductor)
+    {
+        //
+    }
+
+    public function update(Request $request, Conductor $conductor)
+    {
+        //
+    }
+
+    public function destroy(Conductor $conductor)
+    {
+        //
+    }
+}
