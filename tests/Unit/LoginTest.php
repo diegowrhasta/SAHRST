e6c0ddd57045621bcd10f4c74ff8820caf865875
diegowrhasta <diegowrhasta@gmail.com>
2019-10-02 11:11:38 -0400

    Done LoginTest using PHPUnit
    As a security measure, use composer install after pulling. Made a Unit Test for the Login endpoint, made a good request and then did a bad request. Both tests work. In order to test with VS Code easily, download "PHPUnit Test Explorer". In order for the tests to run smoothly make a copy of .env with the name .env.testing, when running tests PHPUnit will override this config file instead of the .env. once that's done, be careful that on this .env.testing file, on the field that says APP_URL the url is pointing towards the environment you plan to test on (It can be localhost, or production server).

---
+++ tests/Unit/LoginTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        //Good Test
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+        ])->json('POST','/api/auth/login',[
+            'email' => 'diegojefe@gmail.com',
+            "password" => 'jefe'
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'token_type' => 'Bearer',
+            ]);
+
+        //Bad Test
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+        ])->json('POST','/api/auth/login',[
+            'email' => 'diegojefe@gmail.com',
+            "password" => 'yeet'
+        ]);
+
+        $response
+            ->assertStatus(401)
+            ->assertJson([
+                'message' => 'Unauthorized',
+            ]);
+    }
+}
